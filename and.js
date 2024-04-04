0fade2b4fe38d4788eac95ef5564dc2345c5b084
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-04-04 11:45:29 -0300

    Add files via upload

--- and.js
+++ and.js
@@ -1,1 +1,8 @@
-//Exemplo de uso do operador &&
+// Exemplo de uso do operador &&
+let idade = 25;
+let possuiCarteiradeMotorista = true;
+if(idade >= 18 && possuiCarteiradeMotorista) {
+    console.log("Você pode dirigir.");
+} else {
+    console.log("Você não pode dirigir.");
+}
