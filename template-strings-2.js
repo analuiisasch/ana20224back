5f5c9d39dbbc2022f4554820046202d2d2133d05
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-04-16 09:30:46 -0300

    Add files via upload

---
+++ template-strings-2.js
@@ -0,0 +1,7 @@
+const nome = "valter";
+const idade = 2024-1980;
+ const cidade = "Campo Mourão"
+ const apresentacao = "meu nome é: " + nome + "minha idade é"
+ + idade = ", e nasci na cidade de "
+ + cidade + ".";
+ console.log(apresentacao)
