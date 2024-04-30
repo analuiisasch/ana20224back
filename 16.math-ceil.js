f6aafbf74a14901f477dcd14f0efdc1ae93b9311
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-04-30 09:50:10 -0300

    Add files via upload

---
+++ 16.math-ceil.js
@@ -0,0 +1,6 @@
+//O Math.ceil() é uma função em JavaScript que arredonda um número para cima
+
+let numero = 10.2;
+let numeroArredondado = Math.ceil(numero);
+
+console.log(numeroArredondado);//Sída: 11
