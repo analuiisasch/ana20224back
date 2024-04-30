bd2e4cf4896cfa2957fa5217e3b36ebb4c6854f3
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-04-30 10:53:39 -0300

    Add files via upload

---
+++ 19-math.sqrt.js
@@ -0,0 +1,6 @@
+//Math.sqrt(): Esta função retorna a raiz quadrada de um nuḿero.
+//A função sqrt() (abreviação de "square root", que significa raiz quadrada )
+
+let raizQuadrada = Math.sqrt(9);
+
+console.log(raizQuadrada);//Saída: 3
