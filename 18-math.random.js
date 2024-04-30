ca4df58fbcb7bed4a147a61060bdb061c1cb9b57
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-04-30 10:45:11 -0300

    Add files via upload

---
+++ 18-math.random.js
@@ -0,0 +1,5 @@
+//Math.random(): Esta função retorna um número pseusoaleatório entre 0 (inclu)
+
+let numeroAleatorio = Math.random();
+
+console.log(numeroAleatorio);//Saída: um numero aleatório entre 0 e 1
